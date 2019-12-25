a9bd8af3521ba64c44f6e34d0444bc0b855c5dc8
MSDOMAIN1\wchen3 <wenguang.chen@morningstar.com>
2019-12-25 11:03:18 +0800

    add

---
+++ config-client/src/main/java/com/morningstar/cloud/configclient/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.morningstar.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wenguang
+ * @create 2019 - 12 - 25 10:58.
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
